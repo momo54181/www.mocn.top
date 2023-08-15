ec738e63d7ca8a41fe77a892855fbe40cb889539
Momo <momo5418@outlook.com>
2023-08-15 12:21:41 +0800

    1

--- src/components/Links/index.vue
+++ src/components/Links/index.vue
@@ -54,8 +54,8 @@ import {
   CompactDisc,
   Cloud,
   Compass,
-  AnimalCat20,
-  ChannelShare12,
+  AnimalCat,
+  ChannelShare,
   LaptopCode,
 } from "@vicons/fa";
 import { mainStore } from "@/store";
@@ -83,8 +83,8 @@ const siteIcon = {
   Cloud,
   CompactDisc,
   Compass,
-  AnimalCat20,
-  ChannelShare12,
+  AnimalCat,
+  ChannelShare,
   LaptopCode,
 };
 
